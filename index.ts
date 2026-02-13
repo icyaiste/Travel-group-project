82e17bac15f24854d45d91b9586337512ea58503
Priti Saxena <saxenapriti1008@gmail.com>
2026-02-13 20:20:14 +0100

    Added Types
    Added interfaces countryInfo:
    name
     capital
     currecny
    flag
    region
     population
    languages

---
+++ index.ts
@@ -0,0 +1,13 @@
+// Types
+
+interface countryInfo {
+  name : string;
+  capital : string;
+  currency : string;
+  flag : string;
+  regoin : string;
+  population : number;
+  languages : string[];
+
+}
+
